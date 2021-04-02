39fc2d9531d9706a5685c74e6f51fedd26f3f1f7
gabrieljreed <gabrieljreed@gmail.com>
2021-04-01 21:50:05 -0600

    Finished many to one assignment

--- todo-list-with-projects-main/back-end/server.js
+++ todo-list-with-projects-main/back-end/server.js
@@ -25,6 +25,17 @@ const projectSchema = new mongoose.Schema({
 // Create a model for projects
 const Project = mongoose.model('Project', projectSchema);
 
+const itemSchema = new mongoose.Schema({
+    project: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Project'
+    },
+    text: String,
+    completed: Boolean,
+});
+
+const Item = mongoose.model('Item',itemSchema);
+
 
 // Create a project
 app.post('/api/projects', async(req, res) => {
@@ -41,6 +52,87 @@ app.post('/api/projects', async(req, res) => {
     }
 });
 
+app.get("/api/projects", async(req, res) => {
+    try {
+        let projects = await Project.find();
+        res.send(projects);
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.post('/api/projects/:projectID/items', async (req, res) => {
+    try {
+        let project = await Project.findOne({_id: req.params.projectID});
+        if (!project) {
+            res.sendStatus(404);
+            return;
+        }
+        let item = new Item({
+            project: project,
+            text: req.body.text,
+            completed: req.body.completed,
+        });
+        await item.save();
+        res.send(item);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+// Get a list of items
+app.get("/api/projects/:projectID/items", async(req, res) => {
+    try {
+        let project = await Project.findOne({_id: req.params.projectID});
+        if (!project) {
+            res.sendStatus(404);
+            return;
+        }
+        let items = await Item.find({project: project});
+        res.send(items);
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+// Update an item
+app.put("/api/projects/:projectID/items/:itemID", async(req, res) => {
+    try {
+        let item = await Item.findOne({_id:req.params.itemID, project: req.params.projectID});
+        if(!item) {
+            res.sendStatus(404);
+            return;
+        }
+
+        item.text = req.body.text;
+        item.completed = req.body.completed;
+        await item.save();
+        res.send(item);
+    } catch(error) {
+        console.log(error);
+        res.send(500);
+    }
+});
+
+// Delete an item
+app.put("/api/projects/:projectID/items/:itemID", async(req, res) => {
+    try {
+        let item = await Item.findOne({_id:req.params.itemID, project: req.params.projectID});
+        if(!item) {
+            res.sendStatus(404);
+            return;
+        }
+
+        await item.delete();
+        res.sendStatus(200);
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
 
 
 app.listen(3000, () => console.log('Server listening on port 3000 ;)'));
